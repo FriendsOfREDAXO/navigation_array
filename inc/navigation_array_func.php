44999df06b88a6caa7c38dd259332d64cf44a2db
Thomas Skerbis <thomas.skerbis@mac.com>
2023-12-10 10:37:26 +0100

    Update navigation_array_func.php

--- inc/navigation_array_func.php
+++ inc/navigation_array_func.php
@@ -4,9 +4,6 @@ if (!function_exists('navArray')) {
     function navArray($start = 0, $depth = 0, $ignoreOfflines = true)
     {
         $navArray = new FriendsOfRedaxo\navigationArray($start, $depth, $ignoreOfflines);
-        $navigation->setCustomDataCallback(function ($cat) {
-            return ['catObject' => $cat];
-        });
         $result = $navArray->generate();
         return $result;
     }
