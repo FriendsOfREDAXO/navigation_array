d2acc0c900c5b41797f26230da637978aa22a836
Thomas Skerbis <thomas.skerbis@mac.com>
2023-12-10 10:53:26 +0100

    added catObject to navigation_array_func.php

--- inc/navigation_array_func.php
+++ inc/navigation_array_func.php
@@ -4,7 +4,11 @@ if (!function_exists('navArray')) {
     function navArray($start = 0, $depth = 0, $ignoreOfflines = true)
     {
         $navArray = new FriendsOfRedaxo\navigationArray($start, $depth, $ignoreOfflines);
+        $navArray->setCustomDataCallback(function ($cat) {
+            return ['catObject' => $cat];
+        });
         $result = $navArray->generate();
         return $result;
     }
 }
+
