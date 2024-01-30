b1113f33a37925fb14d0c5c2e2e210a8e0cc9c73
Thomas Skerbis <thomas.skerbis@mac.com>
2024-01-30 22:01:09 +0100

    navigation_array_func.php

--- inc/navigation_array_func.php
+++ inc/navigation_array_func.php
@@ -1,9 +1,11 @@
 <?php
+use FriendsOfRedaxo\navigationArray\buildArray;
+
 // Function zur Generierung eines Navigationsarrays
 if (!function_exists('navArray')) {
     function navArray($start = -1, $depth = 0, $ignoreOfflines = true)
     {
-        $navArray = new FriendsOfRedaxo\navigationArray($start, $depth, $ignoreOfflines);
+        $navArray = new buildArray($start, $depth, $ignoreOfflines);
         $navArray->setCustomDataCallback(function ($cat) {
             return ['catObject' => $cat];
         });
