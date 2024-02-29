41dca8a4979c1d24fa6a9be798300f9137431fff
Thomas Skerbis <thomas.skerbis@mac.com>
2024-02-29 10:53:04 +0100

    Update boot.php
    Co-authored-by: Thomas Blum <thomas@redaxo.org>

--- boot.php
+++ boot.php
@@ -5,5 +5,5 @@ use rex_addon;
 use rex_extension;
 
 rex_extension::register('PACKAGES_INCLUDED', function($ep) {
-        include_once rex_path::addon('navigation_array/inc').'navigation_array_func.php';
+    include_once rex_path::addon('navigation_array', 'inc/navigation_array_func.php');
 });
