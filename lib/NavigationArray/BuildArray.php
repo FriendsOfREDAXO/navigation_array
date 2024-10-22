19e5d9e8d1ed3a419b856fd634a0912f02cf1227
Marco Hanke <marco@36pixel.de>
2024-10-22 22:55:14 +0200

    add phpdocs and rearrange code

--- lib/NavigationArray/BuildArray.php
+++ lib/NavigationArray/BuildArray.php
@@ -28,7 +28,7 @@ class BuildArray
     private $startCats;
     private $excludedCategories = []; // Neue Eigenschaft
 
-    public function __construct($start = -1, $depth = 4, $ignoreOfflines = true, $depthSaved = 0, $level = 0)
+    public function __construct(int $start = -1, int $depth = 4, bool $ignoreOfflines = true, $depthSaved = 0, int $level = 0)
     {
         $this->start = $start;
         $this->depth = $depth;
@@ -37,36 +37,59 @@ class BuildArray
         $this->level = $level;
     }
 
-    public function setExcludedCategories($excludedCategories): self
+    /**
+     * Set categories to exclude from the navigation (int or array of ints with category ids).
+     *
+     * @param int|array $excludedCategories
+     * @return $this
+     */
+    public function setExcludedCategories(int|array $excludedCategories): self
     {
         if (is_int($excludedCategories)) {
             $excludedCategories = [$excludedCategories];
         }
 
-        if (is_array($excludedCategories)) {
-            $this->excludedCategories = $excludedCategories;
-        } else {
+        if (!is_array($excludedCategories)) {
             $message = 'Excluded categories must be an integer or an array of integers.';
             rex_logger::logError(E_USER_ERROR, $message, __FILE__, __LINE__);
             throw new rex_exception($message);
         }
 
+        $this->excludedCategories = $excludedCategories;
         return $this;
     }
 
-    public function setStart($start): self
+    /**
+     * Set ID of the category to start with (default: -1, yrewrite mountID or root category).
+     *
+     * @param int $start
+     * @return $this
+     */
+    public function setStart(int $start): self
     {
         $this->start = $start;
         return $this;
     }
 
-    public function setDepth($depth): self
+    /**
+     * Set how many levels should the navigation show (default: 4).
+     *
+     * @param int $depth
+     * @return $this
+     */
+    public function setDepth(int $depth): self
     {
         $this->depth = $depth;
         return $this;
     }
 
-    public function setIgnore($ignore): self
+    /**
+     * Set whether offline categories should be ignored (default: true).
+     *
+     * @param bool $ignore
+     * @return $this
+     */
+    public function setIgnore(int $ignore): self
     {
         $this->ignoreOfflines = $ignore;
         return $this;
@@ -78,17 +101,31 @@ class BuildArray
         return $this;
     }
 
-    public function setLevel($lvl): self
+    /**
+     * @param int $lvl
+     * @return $this
+     */
+    public function setLevel(int $lvl): self
     {
         $this->level = $lvl;
         return $this;
     }
 
+    /**
+     * Create a new instance of BuildArray.
+     *
+     * @return self
+     */
     public static function create(): self
     {
         return new self();
     }
 
+    /**
+     * Generate the navigation array.
+     *
+     * @return array
+     */
     public function generate(): array
     {
         $result = [];
@@ -99,44 +136,64 @@ class BuildArray
         $this->initializeStartCategory();
 
         foreach ($this->startCats as $cat) {
+            //check ycom permissions
             if (!$this->isCategoryPermitted($cat)) {
                 continue;
             }
-
-            // Ausschlusslogik prüfen
+            //check excluded categories
             if (in_array($cat->getId(), $this->excludedCategories)) {
                 continue;
             }
-
+            //check category filter callback
             if (is_callable($this->categoryFilterCallback) && !call_user_func($this->categoryFilterCallback, $cat)) {
                 continue;
             }
-
             $result[] = $this->processCategory($cat, $currentCatpath, $currentCat_id);
         }
         return array_filter($result);
     }
 
+    /**
+     * Set a callback to filter categories.
+     *
+     * @param callable $callback
+     * @return $this
+     */
     public function setCategoryFilterCallback(callable $callback): self
     {
         $this->categoryFilterCallback = $callback;
         return $this;
     }
 
+    /**
+     * Set a callback to add custom data to the category array.
+     *
+     * @param callable $callback
+     * @return $this
+     */
     public function setCustomDataCallback(callable $callback): self
     {
         $this->customDataCallback = $callback;
         return $this;
     }
 
+    /**
+     * Generate the navigation array as JSON.
+     *
+     * @return string
+     */
     public function toJson(): string
     {
         $array = $this->generate();
         return json_encode($array, JSON_PRETTY_PRINT);
     }
 
+    /**
+     * @return void
+     */
     private function initializeStartCategory(): void
     {
+        // get yrewrite mount id if available
         if (is_int($this->start) && $this->start == -1 && rex_addon::get('yrewrite')->isAvailable()) {
             $this->start = rex_yrewrite::getDomainByArticleId(rex_article::getCurrentId(), rex_clang::getCurrentId())->getMountId();
         } elseif ($this->start == -1) {
@@ -164,13 +221,25 @@ class BuildArray
         }
     }
 
-    private function isCategoryPermitted($cat): bool
+    /**
+     * Check if the category is permitted by ycom.
+     *
+     * @param rex_category $cat
+     * @return bool
+     */
+    private function isCategoryPermitted(rex_category $cat): bool
     {
         $ycom_check = rex_addon::get('ycom')->getPlugin('auth')->isAvailable();
         return !$ycom_check || $cat->isPermitted();
     }
 
-    private function processCategory($cat, $currentCatpath, $currentCat_id): array
+    /**
+     * @param rex_category $cat
+     * @param array $currentCatpath
+     * @param int $currentCat_id
+     * @return array
+     */
+    private function processCategory(rex_category $cat, array $currentCatpath, int $currentCat_id): array
     {
         if ($this->level > $this->depth) {
             return [];
@@ -178,7 +247,7 @@ class BuildArray
 
         $catId = $cat->getId();
 
-        // Unterkategorien prüfen und ggf. ausschließen
+        //check subcategories and exclude if depth is reached
         $children = [];
         if ($this->level <= $this->depth && $cat->getChildren($this->ignoreOfflines)) {
             $childCats = $cat->getChildren($this->ignoreOfflines);
@@ -212,7 +281,11 @@ class BuildArray
         return $categoryArray;
     }
 
-    private function generateSubCategories($parentCat): array
+    /**
+     * @param $parentCat
+     * @return array
+     */
+    private function generateSubCategories(rex_category $parentCat): array
     {
         $originalStart = $this->start;
         $this->start = $parentCat->getId();
