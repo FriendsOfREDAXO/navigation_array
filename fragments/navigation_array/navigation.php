d50041ce95120c81f60a574c2f5e81af05a6a335
Marco Hanke <marco@36pixel.de>
2024-10-22 22:13:43 +0200

    update fragment

--- fragments/navigation_array/navigation.php
+++ fragments/navigation_array/navigation.php
@@ -1,5 +1,14 @@
-<nav>
-    <ul role="menubar">
-        <?= $this->subfragment('navigation_array/navigationElements.php'); ?>
-    </ul>
-</nav>
+<?php foreach ($this->getVar('navigationArray') as $categoryArray): ?>
+    <li>
+        <a class="<?= $categoryArray['active'] ? $this->getVar('activeClass') : '' ?>" role="menuitem" href="<?= rex_escape($categoryArray['url']) ?>">
+            <?= $categoryArray['catName'] ?>
+        </a>
+        <?php if (!empty($categoryArray['children'])): ?>
+            <ul role="menu">
+                <?php
+                $this->subfragment('navigation_array/navigation.php', ['navigationArray' => $categoryArray['children']]);
+                ?>
+            </ul>
+        <?php endif; ?>
+    </li>
+<?php endforeach; ?>
